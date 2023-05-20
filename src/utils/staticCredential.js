67fb8e31a7b7920cee8ca1a7c1d785bad087ab97
igooplay <75844668+igooplay@users.noreply.github.com>
2023-05-20 11:09:06 -0300

    Update staticCredential.js

--- src/utils/staticCredential.js
+++ src/utils/staticCredential.js
@@ -1,13 +1,13 @@
 export const google_client_id = "GOGGLE_CLIENT_ID";
 export const fb_app_id = "YOUR_FB_APP_ID";
 export const firebaseConfigData = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyCc9xWmBq8bx7pJbulmyOSXcUNI7xDlRUI",
+  authDomain: "voubedelivery.firebaseapp.com",
+  projectId: "voubedelivery",
+  storageBucket: "voubedelivery.appspot.com",
+  messagingSenderId: "528852790709",
+  appId: "1:528852790709:web:130f60a3248d7d69bc3dfc",
+  measurementId: "G-F6BQL92T9M"
 };
 
 export const appleLoginCredential = {
